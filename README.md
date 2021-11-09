2564e7c1a89324ce5698692585bce3269538cf39
lifachao <2390210930@qq.com>
2021-11-09 18:19:58 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,10 @@
+# 概述
+
+- vite+vue3.0+antd+vuex+router+ts 全家桶探索学习
+
+# 项目搭建
+
+```
+  // vite初始化
+  npm init @vitejs/app my-vue-app --template vue-ts
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: { type: String },
+  },
+});
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>

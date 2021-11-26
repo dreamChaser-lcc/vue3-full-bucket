ce56fa76809fd484fdbda7c55720bb4975cbc3b6
lifachao <2390210930@qq.com>
2021-11-26 17:40:46 +0800

    修改prettier格式化规则

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,10 @@
 module.exports = {
+  useTabs: true,
   trailingComma: "es5", // 尾随逗号
   tabWidth: 2, // tab两个空格
   semi: true, // 末尾加分号
   singleQuote: false,
-  bracketSpacing: true,
-  bracketSameLine: true,
+  bracketSameLine: false, // 标签最后>单独换行
+  printWidth: 100, // 单行支持最大
+  bracketSpacing: true, // 对象间加空格 {a: 'das'}
 };
